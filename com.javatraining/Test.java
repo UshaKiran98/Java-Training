e86964abd04675336f2d56e81d061f65968155f2
UshaKiran98 <ushamallavarapu98@gmail.com>
2023-04-17 18:41:15 +0530

    This is first program on calculator App

---
+++ com.javatraining/Test.java
@@ -0,0 +1,25 @@
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		int a=200;
+		int b=100;
+		
+		Maths m = new Maths();
+		
+		m.add(a, b);
+
+		m.multiply(a, b);
+		
+		m.substract(a, b);
+		
+		m.division(a, b);
+		
+		m.square(a);
+		
+		m.cube(a);
+		
+	}
+
+}
